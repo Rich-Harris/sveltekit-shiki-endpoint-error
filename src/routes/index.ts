80167fd81721937d3de3c5a4830358af7547d20c
Rich Harris <hello@rich-harris.dev>
2022-05-17 16:28:10 -0400

    try this

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
-import * as shiki from 'shiki'
-import { base } from '$app/paths'
+import * as shiki from 'shiki';
+import { base } from '$app/paths';
 
-export async function get() {
-  shiki.setCDN(`${base}/shiki/`)
-  const highlighter = await shiki.getHighlighter({ theme: 'github-dark' })
-  return {
-    body: { data: highlighter.codeToHtml(`console.log('shiki');`, { lang: 'js' }) },
-  }
+export async function get({ url }) {
+	shiki.setCDN(`${url.origin}/shiki/`);
+	const highlighter = await shiki.getHighlighter({ theme: 'github-dark' });
+	return {
+		body: { data: highlighter.codeToHtml(`console.log('shiki');`, { lang: 'js' }) }
+	};
 }
